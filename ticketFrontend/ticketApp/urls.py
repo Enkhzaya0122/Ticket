a6726a774ecfe3be194cdd77b945d4f3bb1aaf0b
Enkhzaya0221 <sw20d024@mandakh.edu.mn>
2023-05-07 19:05:10 +0800

    web frontend

--- ticketFrontend/ticketApp/urls.py
+++ ticketFrontend/ticketApp/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('login/', views.login),
-    path('headerBase/', views.login),
+    path('', views.index, name="index"),
+    path('login/', views.login, name="login"),
+    path('register/', views.register, name="register"),
+    path('headerBase/', views.headerBase, name="headerBase"),
+    path('detail/', views.detail, name="detail"),
+    
 ]
--- ticketFrontend/ticketApp/views.py
+++ ticketFrontend/ticketApp/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def headerBase(request):
+    return render(request, "base/headerBase.html")
+
 def index(request):
     return render(request, "index.html")
 
 def login(request):
     return render(request, "login.html")
+
+def detail(request):
+    return render(request, "detail.html")
+
+def register(request):
+    return render(request, "register.html")

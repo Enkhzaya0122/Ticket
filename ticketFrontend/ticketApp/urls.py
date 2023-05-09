637ce476b58b5ffcaaf9254cd0b394144e59e9a7
Enkhzaya0221 <sw20d024@mandakh.edu.mn>
2023-05-10 01:43:34 +0800

    ticket image

--- ticketFrontend/ticketApp/urls.py
+++ ticketFrontend/ticketApp/urls.py
@@ -3,13 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('login/', views.login, name="login"),
+    path('', views.login, name="login"),
+    path('index/', views.index, name="index"),
     path('register/', views.register, name="register"),
     path('headerBase/', views.headerBase, name="headerBase"),
     path('detail/<int:id>', views.detail, name="detail"),
     path('ticketAdmin/', views.ticketAdmin, name="ticketAdmin"),
     path('registerTicket/<int:id>', views.registerTicket, name="registerTicket"),
     path('delete/<int:id>', views.delete, name="delete"),
-    
+    path('log_out/', views.log_out, name='log_out'),
+    path('menu/', views.menu, name='menu'),
 ]
--- ticketFrontend/ticketApp/views.py
+++ ticketFrontend/ticketApp/views.py
@@ -1,90 +1,104 @@
 from django.shortcuts import render,redirect
 import requests
+from django.contrib.auth import logout
 import json
 
 def headerBase(request):
     return render(request, "base/headerBase.html")
 
 def index(request):
-    context = {}
-    if request.method == 'GET':
+    if request.session.get('email'):
+        context = {}
+        if request.method == 'GET':
 
-        type = 'All'
-        if request.GET.get('All'):
-            type = "All"
-        if request.GET.get('jujig'):
-            type = "jujig"
-        if request.GET.get('bujig'):
-            type = "bujig"
-        if request.GET.get('concert'):
-            type = "concert"
-        if request.GET.get('duuri'):
-            type = "duuri"
-        if request.GET.get('ballet'):
-            type = "ballet"
-        requestJSON = {
-            "action" : "getTickets",
-            "type" : f"{type}"
-        }
-        response = requests.get('http://127.0.0.1:8080/ticket/',
-                                data=json.dumps(requestJSON),
-                                headers={'Content-Type' : 'application/json'})
-        resp = json.loads(response.text)
-        context = {
-            'data' : 
-            resp['data']
-        }
-    if request.method == 'POST':
-        type = 'All'
-        if request.POST.get('All'):
-            type = "All"
-        if request.POST.get('jujig'):
-            type = "jujig"
-        if request.POST.get('bujig'):
-            type = "bujig"
-        if request.POST.get('concert'):
-            type = "concert"
-        if request.POST.get('duuri'):
-            type = "duuri"
-        if request.POST.get('ballet'):
-            type = "ballet"
-        requestJSON = {
-            "action" : "getTickets",
-            "type" : f"{type}"
-        }
-        response = requests.get('http://127.0.0.1:8080/ticket/',
-                                data=json.dumps(requestJSON),
-                                headers={'Content-Type' : 'application/json'})
-        resp = json.loads(response.text)
-        context = {
-            'data' : 
-            resp['data']
-        }
-    return render(request, "index.html",context=context)
+            type = 'All'
+            if request.GET.get('All'):
+                type = "All"
+            if request.GET.get('jujig'):
+                type = "jujig"
+            if request.GET.get('bujig'):
+                type = "bujig"
+            if request.GET.get('concert'):
+                type = "concert"
+            if request.GET.get('duuri'):
+                type = "duuri"
+            if request.GET.get('ballet'):
+                type = "ballet"
+            requestJSON = {
+                "action" : "getTickets",
+                "type" : f"{type}"
+            }
+            response = requests.get('http://127.0.0.1:8080/ticket/',
+                                    data=json.dumps(requestJSON),
+                                    headers={'Content-Type' : 'application/json'})
+            resp = json.loads(response.text)
+            context = {
+                'data' : 
+                resp['data']
+            }
+        if request.method == 'POST':
+            type = 'All'
+            if request.POST.get('All'):
+                type = "All"
+            if request.POST.get('jujig'):
+                type = "jujig"
+            if request.POST.get('bujig'):
+                type = "bujig"
+            if request.POST.get('concert'):
+                type = "concert"
+            if request.POST.get('duuri'):
+                type = "duuri"
+            if request.POST.get('ballet'):
+                type = "ballet"
+            requestJSON = {
+                "action" : "getTickets",
+                "type" : f"{type}"
+            }
+            response = requests.get('http://127.0.0.1:8080/ticket/',
+                                    data=json.dumps(requestJSON),
+                                    headers={'Content-Type' : 'application/json'})
+            resp = json.loads(response.text)
+            context = {
+                'data' : 
+                resp['data']
+            }
+        return render(request, "index.html",context=context)
+    else:
+        return redirect('login')
 
 def login(request):
-    if request.method == "GET":
-        email = request.GET.get('email')
-        password = request.GET.get('password')
-        if email == None and password == None:
-            return render(request, 'login.html')
-        requestJSON = {
-            "action" : "login",
-            "email" : email,
-            "password" : password
-        }
-        
-    
-        response = requests.get('http://127.0.0.1:8080/user/',
-                                data=json.dumps(requestJSON),
-                                headers={'Content-Type' : 'application/json'})
-        resp = json.loads(response.text)
-        context = {
-            'data' : resp['data']
-        }
-    if resp['data']!= 'user not found':
+    if request.session.get('email'):
         return redirect('index')
-    return render(request, "login.html",context=context)
+    else:
+        if request.method == "GET":
+            email = request.GET.get('email')
+            password = request.GET.get('password')
+            if email == None and password == None:
+                return render(request, 'login.html')
+            requestJSON = {
+                "action" : "login",
+                "email" : email,
+                "password" : password
+            }
+            
+            response = requests.get('http://127.0.0.1:8080/user/',
+                                    data=json.dumps(requestJSON),
+                                    headers={'Content-Type' : 'application/json'})
+            resp = json.loads(response.text)
+            
+            if resp['data']!= 'user not found':
+                # Create a session for the user
+                request.session['email'] = email
+                request.session['password'] = password
+                
+                return redirect('index')
+            
+            context = {
+                'data' : resp['data']
+            }
+            
+        return render(request, "login.html",context=context)
+
 
 def detail(request,id):
     context = {}
@@ -109,104 +123,110 @@ def register(request):
     return render(request, "register.html")
 
 def registerTicket(request,id):
-    context = {}
-    if request.method == 'GET':
-        requestJSON = {
-            "action" : "getTicketInfo",
-            "tnum" : id
-        }
-        response = requests.post('http://127.0.0.1:8080/ticket/',
-                                data=json.dumps(requestJSON),
-                                headers={'Content-Type' : 'application/json'})
-        resp = json.loads(response.text)
-        context = {
-            'data' : resp['data']
-        }
-    if request.method == 'POST':
-        title = request.POST.get('title')
-        desc = request.POST.get('desc')
-        date = request.POST.get('date')
-        location = request.POST.get('location')
-        price = request.POST.get('price')
-        catname = request.POST['catname']
-        requestJSON = {
-            "action" : "registerTicket",
-            "title" : title,
-            "desc" : desc,
-            "date" : date,
-            "location" : location,
-            "price" : price,
-            "catname" : catname,
-            "tnum" : id
-        }
-        response = requests.post('http://127.0.0.1:8080/ticket/',
-                                data=json.dumps(requestJSON),
-                                headers={'Content-Type' : 'application/json'})
-        resp = json.loads(response.text)
-        context = {
-            'data' : resp['data']
-        }
-        if resp['data'] == 'success':
-            return redirect('index')
-    return render(request, "registerTicket.html", context=context)
+    if request.session.get('email'):
+        context = {}
+        if request.method == 'GET':
+            requestJSON = {
+                "action" : "getTicketInfo",
+                "tnum" : id
+            }
+            response = requests.post('http://127.0.0.1:8080/ticket/',
+                                    data=json.dumps(requestJSON),
+                                    headers={'Content-Type' : 'application/json'})
+            resp = json.loads(response.text)
+            context = {
+                'data' : resp['data']
+            }
+        if request.method == 'POST':
+            title = request.POST.get('title')
+            desc = request.POST.get('desc')
+            date = request.POST.get('date')
+            location = request.POST.get('location')
+            price = request.POST.get('price')
+            catname = request.POST['catname']
+            requestJSON = {
+                "action" : "registerTicket",
+                "title" : title,
+                "desc" : desc,
+                "date" : date,
+                "location" : location,
+                "price" : price,
+                "catname" : catname,
+                "tnum" : id
+            }
+            response = requests.post('http://127.0.0.1:8080/ticket/',
+                                    data=json.dumps(requestJSON),
+                                    headers={'Content-Type' : 'application/json'})
+            resp = json.loads(response.text)
+            context = {
+                'data' : resp['data']
+            }
+            if resp['data'] == 'success':
+                return redirect('index')
+        return render(request, "registerTicket.html", context=context)
+    else:
+        return redirect('login')
 
 def ticketAdmin(request):
-    context = {}
-    if request.method == 'GET':
-        
-        type = 'All'
-        if request.GET.get('All'):
-            type = "All"
-        if request.GET.get('jujig'):
-            type = "jujig"
-        if request.GET.get('bujig'):
-            type = "bujig"
-        if request.GET.get('concert'):
-            type = "concert"
-        if request.GET.get('duuri'):
-            type = "duuri"
-        if request.GET.get('ballet'):
-            type = "ballet"
-        requestJSON = {
-            "action" : "getTickets",
-            "type" : f"{type}"
-        }
-        response = requests.get('http://127.0.0.1:8080/ticket/',
-                                data=json.dumps(requestJSON),
-                                headers={'Content-Type' : 'application/json'})
-        resp = json.loads(response.text)
-        context = {
-            'data' : 
-            resp['data']
-        }
-    if request.method == 'POST':
-        type = 'All'
-        if request.POST.get('All'):
-            type = "All"
-        if request.POST.get('jujig'):
-            type = "jujig"
-        if request.POST.get('bujig'):
-            type = "bujig"
-        if request.POST.get('concert'):
-            type = "concert"
-        if request.POST.get('duuri'):
-            type = "duuri"
-        if request.POST.get('ballet'):
-            type = "ballet"
-        requestJSON = {
-            "action" : "getTickets",
-            "type" : f"{type}"
-        }
-        response = requests.get('http://127.0.0.1:8080/ticket/',
-                                data=json.dumps(requestJSON),
-                                headers={'Content-Type' : 'application/json'})
-        resp = json.loads(response.text)
-        context = {
-            'data' : 
-            resp['data']
-        }
-    print(resp)
-    return render(request, "ticketAdmin.html",context=context)
+    if request.session.get('email'):
+        context = {}
+        if request.method == 'GET':
+            
+            type = 'All'
+            if request.GET.get('All'):
+                type = "All"
+            if request.GET.get('jujig'):
+                type = "jujig"
+            if request.GET.get('bujig'):
+                type = "bujig"
+            if request.GET.get('concert'):
+                type = "concert"
+            if request.GET.get('duuri'):
+                type = "duuri"
+            if request.GET.get('ballet'):
+                type = "ballet"
+            requestJSON = {
+                "action" : "getTickets",
+                "type" : f"{type}"
+            }
+            response = requests.get('http://127.0.0.1:8080/ticket/',
+                                    data=json.dumps(requestJSON),
+                                    headers={'Content-Type' : 'application/json'})
+            resp = json.loads(response.text)
+            context = {
+                'data' : 
+                resp['data']
+            }
+        if request.method == 'POST':
+            type = 'All'
+            if request.POST.get('All'):
+                type = "All"
+            if request.POST.get('jujig'):
+                type = "jujig"
+            if request.POST.get('bujig'):
+                type = "bujig"
+            if request.POST.get('concert'):
+                type = "concert"
+            if request.POST.get('duuri'):
+                type = "duuri"
+            if request.POST.get('ballet'):
+                type = "ballet"
+            requestJSON = {
+                "action" : "getTickets",
+                "type" : f"{type}"
+            }
+            response = requests.get('http://127.0.0.1:8080/ticket/',
+                                    data=json.dumps(requestJSON),
+                                    headers={'Content-Type' : 'application/json'})
+            resp = json.loads(response.text)
+            context = {
+                'data' : 
+                resp['data']
+            }
+        print(resp)
+        return render(request, "ticketAdmin.html",context=context)
+    else:
+        return redirect('login')
 
 def delete(request,id):
     if request.method == "POST":
@@ -239,3 +259,10 @@ def delete(request,id):
         }
     return render(request, "delete.html",context=context)
 
+def log_out(request):
+    del request.session['email']
+    logout(request)
+    return redirect('login')
+
+def menu(request):
+    return render(request, "jijig/menu.html")
